7ed93ac3a5a7cb575db17acd5d977fc5d009434c
Daniel Contreras <dantnad@icloud.com>
2023-05-04 21:52:20 -0400

    Initial app configuration Variables configuration

---
+++ src/components/result/result.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Result = ({ amount, unit }) => {
+  return (
+    <>
+      <h2>{amount}</h2>
+      <h3>{unit}</h3>
+    </>
+  );
+};
+
+export default styled`
+  
+`;
